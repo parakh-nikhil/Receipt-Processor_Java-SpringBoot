f3844f8cad96bc392f53a157a69d86bad818bd33
Nikhil Parakh <nikhilparakh@Nikhils-MacBook-Pro-2.local>
2023-09-11 19:53:33 -0400

    models created

--- src/main/java/com/fetch/ReceiptProcessor/ReceiptProcessorApplication.java
+++ src/main/java/com/fetch/ReceiptProcessor/ReceiptProcessorApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ReceiptProcessorApplication {
+	//TODO: add comments for all the data models
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReceiptProcessorApplication.class, args);
--- src/main/java/com/fetch/ReceiptProcessor/Receipts.java
+++ src/main/java/com/fetch/ReceiptProcessor/Receipts.java
@@ -18,3 +18,4 @@ public class Receipts {
         this.receipts.add(receipt);
     }
 }
+
